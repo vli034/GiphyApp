834130dabb2a0a2982dd08ca8165db2306f29b16
vanessali <li.vanessaa@gmail.com>
2019-07-15 19:50:16 -0400

    added new comments and fixed formatting

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 
 const app = {}; // app object
-app.key = 'mKKDAE6V27NrKf6H4az4dw5YZtNbpcyb';
-app.displayGifs=(data)=>{
-    // console.log(data);
-    // for(let i=0; i<data.length; i++){
-    //     console.log(data.images.original_still.url[i]);
-      
-    // };
-
-
+app.key = 'mKKDAE6V27NrKf6H4az4dw5YZtNbpcyb'; // API key to retrieve giphy images 
+app.displayGifs=(data)=>{ // method of the app object to display the gifs
+    
+    // using a for loop to iterate through the array of "giphs" we recieve 
     data.forEach(data => {
-        // console.log(data.images.original_still.url);
         const gifHtml = `
             <div class="gif-box">
                 <div class="img-box">
@@ -18,51 +12,43 @@ app.displayGifs=(data)=>{
             </div>
             <p class="gif-title">${data.title}</p>
             </div>`
-        $('.results').append(gifHtml);
+        $('.results').append(gifHtml); // append to html 
     });
 
 
 };
-app.getGifs = (userInput) => {
-
+app.getGifs = (userInput) => { // another app method that takes in user input 
     $.ajax({
-        url:"https://api.giphy.com/v1/gifs/search",
+        url:"https://api.giphy.com/v1/gifs/search", //AJAX calls to get ovject 
         method:"GET",
         dataType:"json",
-        data:{ // bc we are calling the api to retrieve the data we are getting requires two paramters we are adding it here 
+        data:{ // bc we are calling the api to retrieve the data we are getting requires two parameters we are adding it here 
             api_key: app.key,
-            q: userInput
+            q: userInput // refer to the api documentation for proper parameters 
         }
-    }).then(res => {
-        // console.log("the result of get gifs",res.data);
-        const gifArray = res.data; // passing the data which is an array object 
-        // once we have gifs display gits to page 
-        app.displayGifs(gifArray);
+    }).then(res => { // res refers to result 
+        const gifArray = res.data; // get the data which is an array object 
+        // once we have gifs display giphs display it to the page  aka call that mutherfucking function 
+        app.displayGifs(gifArray); 
     });
 
-}
+};
+// when your app initialize do this...
 app.init = () =>{
+    // call get gifs -- which ulimately gets the gif via AJAX call
     app.getGifs();
-    
+    // then calls the function displayGifs which tells it to iterate through the array object, retrieve only the lionk and append to html
     $('form').on('submit', function(e){
         e.preventDefault();
         $(`.results`).empty(); // empty field  after you click submit to load other resultd
         const userInput =$(`input[type="text"]`).val();
-        app.getGifs(userInput);
-      
-
+        app.getGifs(userInput);// passing the user input in the method of get GIFS
     });
-
-
 };
 
+// when document ready call app.init
+
 $(()=>{
-    app.init(); // call .init() in document.ready() 
-
-    //make api call 
-    //get input from value from user 
-    // spit data back out on page 
-
-
+    app.init(); 
     
 });
